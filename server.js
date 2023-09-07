5882c47ea6f4d9b20267010753a036699b760d7e
unknown <surenbandara7@gmail.com>
2023-09-07 14:04:37 +0530

    Created new branch

--- server.js
+++ server.js
@@ -1,97 +1,168 @@
+//require our websocket library 
+var WebSocketServer = require('ws').Server; 
+
+//creating a websocket server at port 9090 
+var wss = new WebSocketServer({port: 9090}); 
+
+//all connected to the server users 
+var users = {};
+
 const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
-
 const app = express();
-const server = http.createServer(app);
-const io = socketIo(server);
+const port = 3000;
+  
+// Serve static files from the root directory
+app.use(express.static(__dirname));
 
-let totalDataSizeBytes = 0; // Initialize the total data size counter in bytes
-let startTime = null; // Initialize the start time
-const clients = new Map(); // Map to store client-specific data
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+//when a user connects to our sever 
+wss.on('connection', function(connection) {
+  
+   console.log("User connected");
+	
+   //when server gets a message from a connected user 
+   connection.on('message', function(message) { 
+	
+      var data; 
+		
+      //accepting only JSON messages 
+      try { 
+         data = JSON.parse(message); 
+      } catch (e) { 
+         console.log("Invalid JSON"); 
+         data = {}; 
+      }
+		
+      //switching type of the user message 
+      switch (data.type) { 
+         //when a user tries to login
+         case "login": 
+            console.log("User logged", data.name); 
+				
+            //if anyone is logged in with this username then refuse 
+            if(users[data.name]) { 
+               sendTo(connection, { 
+                  type: "login", 
+                  success: false 
+               }); 
+            } else { 
+               //save user connection on the server 
+               users[data.name] = connection; 
+               connection.name = data.name; 
+					
+               sendTo(connection, { 
+                  type: "login", 
+                  success: true 
+               }); 
+            } 
+				
+            break;
+				
+         case "offer": 
+            //for ex. UserA wants to call UserB 
+            console.log("Sending offer to: ", data.name);
+				
+            //if UserB exists then send him offer details 
+            var conn = users[data.name]; 
+				
+            if(conn != null) { 
+               //setting that UserA connected with UserB 
+               connection.otherName = data.name; 
+					
+               sendTo(conn, { 
+                  type: "offer", 
+                  offer: data.offer, 
+                  name: connection.name 
+               }); 
+            }
+				
+            break;
+				
+         case "answer": 
+            console.log("Sending answer to: ", data.name); 
+            //for ex. UserB answers UserA 
+            var conn = users[data.name]; 
+				
+            if(conn != null) { 
+               connection.otherName = data.name; 
+               sendTo(conn, { 
+                  type: "answer", 
+                  answer: data.answer 
+               }); 
+            } 
+				
+            break; 
+				
+         case "candidate": 
+            console.log("Sending candidate to:",data.name); 
+            var conn = users[data.name];
+				
+            if(conn != null) { 
+               sendTo(conn, { 
+                  type: "candidate", 
+                  candidate: data.candidate 
+               }); 
+            } 
+				
+            break;
+				
+         case "leave": 
+            console.log("Disconnecting from", data.name); 
+            var conn = users[data.name]; 
+            conn.otherName = null; 
+				
+            //notify the other user so he can disconnect his peer connection 
+            if(conn != null) {
+               sendTo(conn, { 
+                  type: "leave" 
+              }); 
+            }
+				
+            break;
+				
+         default: 
+            sendTo(connection, { 
+               type: "error", 
+               message: "Command not found: " + data.type 
+            }); 
+				
+            break; 
+      }
+		
+   }); 
+	
+   //when user exits, for example closes a browser window 
+   //this may help if we are still in "offer","answer" or "candidate" state 
+   connection.on("close", function() { 
+	
+      if(connection.name) { 
+         delete users[connection.name]; 
+			
+         if(connection.otherName) { 
+            console.log("Disconnecting from ", connection.otherName); 
+            var conn = users[connection.otherName]; 
+            conn.otherName = null;
+				
+            if(conn != null) { 
+               sendTo(conn, { 
+                  type: "leave" 
+               }); 
+            }
+         } 
+      }
+		
+   });  
+	
+  // connection.send("Hello world");  
 });
-
-// Function to convert bytes to megabytes (MB)
-function bytesToMB(bytes) {
-    return (bytes / (1024 * 1024)).toFixed(2); // Convert to MB and round to 2 decimal places
+  
+function sendTo(connection, message) { 
+   connection.send(JSON.stringify(message)); 
 }
 
-io.on('connection', (socket) => {
-    console.log('A user connected');
 
-    // Store client-specific data
-    clients.set(socket.id, {
-        totalDataSizeBytes: 0,
-        startTime: null,
-    });
 
-    // Handle incoming video streams from clients
-    socket.on('stream', (stream) => {
-        // if (!startTime) {
-        //     // Record the start time when the first stream is received
-        //     startTime = Date.now();
-        // }
-
-        // Get the client's specific data
-        const clientData = clients.get(socket.id);
-
-        // // Check if stream.imageDataPart is defined and not empty
-        // if (stream.imageDataPart && stream.imageDataPart.length > 0) {
-        //     // Get the size of the incoming data in bytes
-        //     const dataSizeBytes = Buffer.from(stream.imageDataPart, 'base64').length;
-
-        //     // Increment the client's total data size counter in bytes
-        //     clientData.totalDataSizeBytes += dataSizeBytes;
-
-        //     // Increment the global total data size counter in bytes
-        //     totalDataSizeBytes += dataSizeBytes;
-
-        //     // Convert the data size to megabytes (MB)
-        //     const dataSizeMB = bytesToMB(dataSizeBytes);
-
-        //     // Calculate the elapsed time in seconds
-        //     const currentTime = Date.now();
-        //     const elapsedTimeSeconds = (currentTime - startTime) / 1000;
-
-        //     if (elapsedTimeSeconds > 0) {
-        //         // Calculate the data transfer rate for this client in Mb/s
-        //         const dataTransferRateMbPerSec = (bytesToMB(clientData.totalDataSizeBytes) / elapsedTimeSeconds).toFixed(2);
-
-        //         // Log the received data size, total data size, and data transfer rate in Mb/s for this client
-        //         console.log(`Client ${socket.id}: Received data size: ${dataSizeMB} MB | Total data size: ${bytesToMB(clientData.totalDataSizeBytes)} MB | Data Transfer Rate: ${dataTransferRateMbPerSec} Mb/s`);
-        //     }
-
-            // Broadcast the stream to all connected clients except the sender
-            const now = new Date();
-
-            // Extract the current hour, minute, and second
-            const hours = now.getHours();
-            const minutes = now.getMinutes();
-            const seconds = now.getSeconds();
-
-            // Calculate the timestamp in seconds
-            const timestamp = Date.now();
-
-            if (stream.timestamp > timestamp - 3000) {
-                socket.broadcast.emit('stream', stream);
-            }
-         else {
-            console.error('Invalid or empty ');
-        }
-    });
-
-    socket.on('disconnect', () => {
-        console.log('A user disconnected');
-
-        // Remove client-specific data when a client disconnects
-        clients.delete(socket.id);
-    });
-});
-
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
 });
